03873133db1038feea4672750d9657102fa7d465
shift-runstop <shift-runstop@0xFF>
2021-12-08 21:00:58 +0000

    Excerise 1 - API lab 2

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 app.use('/api/movies/', moviesRouter);
+app.use('/api/genres', genresRouter);
 app.use(express.json);
 app.listen(port, () => {
     console.info(`Server running @ ${port}`);
