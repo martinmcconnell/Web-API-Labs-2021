c1d39c974be4415afec99488d35ea46282fe8bea
shift-runstop <shift-runstop@0xFF>
2021-12-09 12:02:30 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 import './db';
 import './seedData'
 
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT;
 app.use('/api/movies/', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
 app.use(express.json);
 app.listen(port, () => {
     console.info(`Server running @ ${port}`);
